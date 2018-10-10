27aa5f35a31ef153962741a9a75e07e29cdc04ab
hermansen <hermansendesigns@gmail.com>
2018-10-10 21:59:31 +0200

    Added Login

--- SomeonePays/src/app/app.component.ts
+++ SomeonePays/src/app/app.component.ts
@@ -1,26 +1,31 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Plugins } from "@capacitor/core";
 
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Platform } from "@ionic/angular";
+
+import * as firebase from "firebase/app";
+import { firebaseConfig } from "./credentials";
+
+const { SplashScreen, StatusBar } = Plugins;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html'
+  selector: "app-root",
+  templateUrl: "app.component.html"
 })
 export class AppComponent {
-  constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
-  ) {
+  constructor(private platform: Platform) {
     this.initializeApp();
   }
 
   initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
+    firebase.initializeApp(firebaseConfig);
+
+    SplashScreen.hide().catch(error => {
+      console.error(error);
+    });
+
+    StatusBar.hide().catch(error => {
+      console.log(error);
     });
   }
 }
---
+++ SomeonePays/src/app/credentials.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBU2oXrDkvUDZRgrOeLosCxitwu4b7p75g",
+  authDomain: "someonepays.firebaseapp.com",
+  databaseURL: "https://someonepays.firebaseio.com",
+  projectId: "someonepays",
+  storageBucket: "someonepays.appspot.com",
+  messagingSenderId: "410415685742"
+};
