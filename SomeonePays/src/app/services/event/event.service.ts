c17e905d528ca69d635755905f4bb53c96531479
hermansen <hermansendesigns@gmail.com>
2018-10-11 09:47:49 +0200

    Added events

--- SomeonePays/src/app/services/event/event.service.ts
+++ SomeonePays/src/app/services/event/event.service.ts
@@ -1,9 +1,44 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import * as firebase from "firebase";
+import "firebase/auth";
+import "firebase/firestore";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class EventService {
+  public eventListRef: firebase.firestore.CollectionReference;
 
-  constructor() { }
+  constructor() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.eventListRef = firebase
+          .firestore()
+          .collection(`/userProfile/${user.uid}/eventList`);
+      }
+    });
+  }
+
+  createEvent(
+    eventName: string,
+    eventDate: Date,
+    eventPrice: number,
+    eventCost: number
+  ): Promise<firebase.firestore.DocumentReference> {
+    return this.eventListRef.add({
+      name: eventName,
+      date: eventDate.toDateString(),
+      price: eventPrice * 1,
+      cost: eventCost * 1,
+      revenue: eventCost * 1
+    });
+  }
+
+  getEventList(): firebase.firestore.CollectionReference {
+    return this.eventListRef;
+  }
+
+  getEventDetail(eventId: string): firebase.firestore.DocumentReference {
+    return this.eventListRef.doc(eventId);
+  }
 }
